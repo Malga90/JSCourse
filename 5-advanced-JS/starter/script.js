951f8333335b5ac739d9e2157378953e4fb88746
Malga90 <savemeebarry@gmail.com>
2019-01-15 16:16:41 +0100

    iife

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -161,3 +161,22 @@ var otherQuestion = interviewQuestion('JS wizard');
 otherQuestion('Malga');
 
 interviewQuestion('teacher')('Mike');
+
+// iife 
+
+function game() {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+}
+game();
+
+(function () {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+})();
+
+(function (goodLuck) {
+    var score = Math.random() * 10;
+    console.log(score >= 5 - goodLuck);
+})(5);
+
