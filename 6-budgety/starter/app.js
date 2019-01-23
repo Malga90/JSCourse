4c3267ea02c032fd0c5c4e40f722d82ed66f87ff
Malga90 <savemeebarry@gmail.com>
2019-01-23 21:28:31 +0100

    creating initialization function

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -40,12 +40,23 @@ var UIController = (function() {
 // Global app controller
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMstrings(); 
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings(); 
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+            
+            if(event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    }
+
 
     var ctrlAddItem = function() {
         // 1. Get the field input data
         var input = UICtrl.getInput();
-        console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -57,13 +68,13 @@ var controller = (function(budgetCtrl, UICtrl) {
         
     }
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        
-        if(event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log('startttt');
+            setupEventListeners();
         }
-    });
+    }
 
 })(budgetController, UIController);
+
+controller.init();
