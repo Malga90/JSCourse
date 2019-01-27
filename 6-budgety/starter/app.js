b1bffe772e475661da174d093e45840fdcbb93c5
Malga90 <savemeebarry@gmail.com>
2019-01-27 20:10:48 +0100

    clearing out input fields

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -104,6 +104,20 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        clearFields: function() {
+            var fields, fieldsArr; 
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ' ,' + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current) {
+                 current.value = '';
+            });
+
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -139,9 +153,11 @@ var controller = (function(budgetCtrl, UICtrl) {
         newItem = budgetController.addItem(input.type, input.description, input.value);
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
-        // 4. Calculate the budget
+        // 4. Clear the fields 
+        UICtrl.clearFields();
+        // 5. Calculate the budget
 
-        // 5. Display the budget on the UI
+        // 6. Display the budget on the UI
         
     }
 
