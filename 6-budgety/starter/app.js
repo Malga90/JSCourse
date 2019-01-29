f42882985ea0f6d3bc1be9b815dd3112e88a327b
Malga90 <savemeebarry@gmail.com>
2019-01-29 20:57:27 +0100

    updating percentages: budget controller

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -6,7 +6,21 @@ var budgetController = (function() {
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
     };
+
+    Expense.prototype.calcPercentage = function(totalIncome) {
+        if (totalIncome > 0) {
+            this.percentage = Math.round((this.value / totalIncome) * 100);
+        } else {
+            this.percentage = -1; 
+        }
+        
+    };
+
+    Expense.prototype.getPercentage = function() {
+        return this.percentage;
+    };      
     
     var Income = function(id, description, value) {
         this.id = id;
@@ -21,6 +35,7 @@ var budgetController = (function() {
         });
         data.totals[type] = sum;
     };
+
     
     var data = {
         allItems: {
@@ -89,6 +104,21 @@ var budgetController = (function() {
                 data.percentage = -1; 
             }
             
+        },
+
+        calculatePercentages: function() {
+            data.allItems.exp.forEach(function(curr){
+                curr.calcPercentage(data.totals.inc);
+            });
+
+        },
+
+        getPercentage: function() {
+            var allPercentages = data.allItems.exp.map(function(curr) {
+                return curr.getPercentage();
+            });
+
+            return allPercentages;
         },
 
         getBudget: function() {
@@ -237,10 +267,11 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     var updatePercentages = function() {
         // Calculate the percentages
-
+        budgetCtrl.calculatePercentages();
         // Read the percentages from budget controller
-
+        var percentages = budgetCtrl.getPercentage();
         // Display percentages in the UI
+        console.log(percentages);
     };
 
 
