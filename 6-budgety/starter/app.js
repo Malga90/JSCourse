5e5cb9b8804921def52343d73d63ceedf5775105
Malga90 <savemeebarry@gmail.com>
2019-01-31 09:42:36 +0100

    displaying the current month and year

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -155,7 +155,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
 
     };
 
@@ -269,6 +270,19 @@ var UIController = (function() {
             });
         },
 
+        displayMonth: function() {
+            var now, year, month, months;
+
+            now = new Date();
+
+            months = ['January', ' February', 'March', 'April', 'May', 'June', 'July', 'Auhust', 'September',
+                      'October', 'November', 'December'];
+            month = now.getMonth();
+
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -369,6 +383,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('startttt');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
@@ -376,6 +391,7 @@ var controller = (function(budgetCtrl, UICtrl) {
                 percentage: -1
             });
             setupEventListeners();
+
         }
     }
 
