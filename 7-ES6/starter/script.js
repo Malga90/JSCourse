2cde9d234e6ce411c45ecdb2cc3cfd9829e14705
Malga90 <savemeebarry@gmail.com>
2019-03-17 21:32:51 +0100

    strings in ES6

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -47,3 +47,58 @@ for (let i = 0; i < 5; i++) {
 }
 
 console.log(i);
+
+// Blocks and IIFEs
+
+// ES6
+{
+  const a = 1;
+  let b = 2;
+  var c = 3;
+}
+
+//console.log(a, b); // error - cause they're not accessible from outside the block
+console.log(c); // it doesn't occure variables
+
+// ES5
+
+(function() {
+  var c = 3;
+})();
+
+//console.log(c);
+
+// STRINGS
+
+let firstName = "John";
+let lastName = "Smith";
+const yearOfBirth = 1987;
+
+function calcAge(year) {
+  return 2019 - year;
+}
+
+calcAge();
+
+// ES5
+console.log(
+  "This is " +
+    firstName +
+    " " +
+    lastName +
+    ". He was born in " +
+    yearOfBirth +
+    ",so today he is " +
+    calcAge(yearOfBirth) +
+    "."
+);
+
+// ES6
+console.log(`This is ${firstName} ${lastName}.
+He was born in ${yearOfBirth}. Today he is ${calcAge(yearOfBirth)}`);
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith("J"));
+console.log(n.endsWith("th"));
+console.log(n.includes("o"));
+console.log(`${firstName} `.repeat(5));
