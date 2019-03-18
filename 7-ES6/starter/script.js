58a473f9f57b899256aac9858c95520a1384048b
Malga90 <savemeebarry@gmail.com>
2019-03-18 18:42:25 +0100

    arrow functions: lexical this keyword

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -130,3 +130,70 @@ ages6 = years.map((el, index) => {
 });
 
 console.log(ages6);
+
+// arrow functions - 'this' kewyowrd
+
+//ES5
+var box5 = {
+  color: "green",
+  position: 1,
+  clickMe: function() {
+    var self = this;
+    document.querySelector(".green"),
+      addEventListener("click", function() {
+        var str =
+          "This is box number " + self.position + " and it is " + self.color;
+
+        alert(str);
+      });
+  }
+};
+
+// box5.clickMe();
+
+// es6
+
+const box6 = {
+  color: "green",
+  position: 1,
+  clickMe: function() {
+    document.querySelector(".green"),
+      addEventListener("click", () => {
+        var str =
+          "This is box number " + this.position + " and it is " + this.color;
+
+        alert(str);
+      });
+  }
+};
+
+box6.clickMe();
+
+function Person(name) {
+  this.name = name;
+}
+
+// es5
+Person.prototype.myFriends5 = function(friends) {
+  var arr = friends.map(
+    function(el) {
+      return this.name + " is frinds with " + el;
+    }.bind(this)
+  );
+
+  console.log(arr);
+};
+
+var friends = ["Bob", "Jane", "Mark"];
+new Person("Jim").myFriends5(friends);
+
+// es6
+
+Person.prototype.myFriends6 = function(friends) {
+  var arr = friends.map(el => `${this.name} is frinds with ${el}`);
+
+  console.log(arr);
+};
+
+var friends = ["Bob", "Jane", "Mark"];
+new Person("Mike").myFriends6(friends);
