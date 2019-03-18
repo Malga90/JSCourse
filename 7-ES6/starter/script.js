096afca0ea953d57ca34cc57c731a287022e9d77
Malga90 <savemeebarry@gmail.com>
2019-03-18 17:55:18 +0100

    arrow functions

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -102,3 +102,31 @@ console.log(n.startsWith("J"));
 console.log(n.endsWith("th"));
 console.log(n.includes("o"));
 console.log(`${firstName} `.repeat(5));
+
+// Arrow functions
+
+const years = [1990, 1963, 2000, 1939];
+
+// ES5
+
+var ages5 = years.map(function(el) {
+  return 2019 - el;
+});
+console.log(ages5);
+
+// ES6
+let ages6 = years.map(el => 2019 - el);
+
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2019 - el}`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+  const now = new Date().getFullYear();
+  const age = now - el;
+
+  return `Age element ${index + 1}: ${age}`;
+});
+
+console.log(ages6);
